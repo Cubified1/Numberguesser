4b69439f9007df4f0c4b42e40e3005e0996c1f90
Cubified <138412300+Cubified1@users.noreply.github.com>
2025-12-29 12:27:56 -0500

    Added a counter for how many times you've guessed wrong and also added a countdown when the app is closing if the user guessed right or quit on the wrong answer

--- Main.py
+++ Main.py
@@ -2,7 +2,8 @@ import random
 import time
 
 Difficulty = input("What difficulty would you like to guess? Easy, Medium, Hard: ").lower()
-
+Incorrect_Guesses = 0
+Close_Counter = 0
 
 
 if Difficulty == "easy":
@@ -27,9 +28,26 @@ else:
 
 while not user_guess == Correct_Number:
     print("wrong guess, try again")
-    quit = input("Do you want to quit? (y/n): ")
+    Incorrect_Guesses += 1
+    if Incorrect_Guesses == 1:
+        print(f"you've guessed wrong {Incorrect_Guesses} time")
+    else:
+        print(f"you've guessed wrong {Incorrect_Guesses} times")
+    quit = str(input("Would you like to quit? (y/n)"))
+
+
+
     if quit == "y":
+        for Close_Counter in range(1, 3):
+            print(f"closing in {Close_Counter}")
+            time.sleep(1)
         break
+
+
+    elif Difficulty == "easy":
+        user_guess = int(input("guess the number from 1 to 50: "))
+    elif Difficulty == "medium":
+        user_guess = int(input("guess the number from 1 to 100: "))
     else:
         user_guess = int(input("guess the number from 1 to 1000: "))
 
@@ -38,6 +56,6 @@ while not user_guess == Correct_Number:
 
 if user_guess == Correct_Number:
     print("Correct")
-
-
-time.sleep(5)
+    for Close_Counter in range(1, 3):
+        print(f"closing in {Close_Counter}")
+        time.sleep(1)
