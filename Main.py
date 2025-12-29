cfd5babb7f541ad352df0306d413a2312d81d20c
Cubified <138412300+Cubified1@users.noreply.github.com>
2025-12-28 21:05:54 -0500

    Added Difficulty levels

--- Main.py
+++ Main.py
@@ -1,10 +1,27 @@
 import random
 import time
 
+Difficulty = input("What difficulty would you like to guess? Easy, Medium, Hard: ")
 
-Correct_Number = random.randint(1, 1000)
 
-user_guess = int(input("guess the number from 1 to 1000: "))
+if Difficulty == "Easy":
+    Correct_Number = random.randint(1, 50)
+
+elif Difficulty == "Medium":
+    Correct_Number = random.randint(50, 100)
+
+else:
+    Correct_Number = random.randint(100, 1000)
+
+
+if Difficulty == "Easy":
+    user_guess = int(input("guess the number from 1 to 50: "))
+
+elif Difficulty == "Medium":
+    user_guess = int(input("guess the number from 1 to 100: "))
+
+else:
+    user_guess = int(input("guess the number from 1 to 1000: "))
 
 while not user_guess == Correct_Number:
     print("wrong guess, try again")
