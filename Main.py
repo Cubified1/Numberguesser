590ab59d53f79f3799f31520e6fd6cd8bd4e1825
Cubified <138412300+Cubified1@users.noreply.github.com>
2025-12-28 21:41:26 -0500

    Added an option to quit on incorrect answer

--- Main.py
+++ Main.py
@@ -1,23 +1,25 @@
 import random
 import time
 
-Difficulty = input("What difficulty would you like to guess? Easy, Medium, Hard: ")
+Difficulty = input("What difficulty would you like to guess? Easy, Medium, Hard: ").lower()
 
 
-if Difficulty == "Easy":
+
+if Difficulty == "easy":
     Correct_Number = random.randint(1, 50)
 
-elif Difficulty == "Medium":
+elif Difficulty == "medium":
     Correct_Number = random.randint(1, 100)
 
 else:
     Correct_Number = random.randint(1, 1000)
 
 
-if Difficulty == "Easy":
+
+if Difficulty == "easy":
     user_guess = int(input("guess the number from 1 to 50: "))
 
-elif Difficulty == "Medium":
+elif Difficulty == "medium":
     user_guess = int(input("guess the number from 1 to 100: "))
 
 else:
@@ -25,7 +27,14 @@ else:
 
 while not user_guess == Correct_Number:
     print("wrong guess, try again")
-    user_guess = int(input("guess the number from 1 to 1000: "))
+    quit = input("Do you want to quit? (y/n): ")
+    if quit == "y":
+        break
+    else:
+        user_guess = int(input("guess the number from 1 to 1000: "))
+
+
+
 
 if user_guess == Correct_Number:
     print("Correct")
